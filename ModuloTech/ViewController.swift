85ae2010fb71053080eeed37f0174b5681aac8f0
Amine <amine.elhajmohamed@gmail.com>
2021-12-01 14:02:12 +0100

    Initial Commit

---
+++ ModuloTech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModuloTech
+//
+//  Created by Mohamed Amine HAJ MOHAMED on 01/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
